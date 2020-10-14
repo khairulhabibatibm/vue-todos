e68d6dc9e2e651fdf0016c4acbfc64b20368f9d8
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2020-10-14 14:27:30 +0700

    change env

--- src/http-common.js
+++ src/http-common.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 export default axios.create({
 
-  baseURL: "http://spring-demo-ocp-git-default.ocp-workshop-fdeea28e4a34dee3e8b3354a9cbfc9f5-0000.jp-tok.containers.appdomain.cloud/api",
-  // baseURL: getEnv('VUE_APP_BE_SERVICE'),
+  // baseURL: "http://spring-demo-ocp-git-default.ocp-workshop-fdeea28e4a34dee3e8b3354a9cbfc9f5-0000.jp-tok.containers.appdomain.cloud/api",
+  baseURL: getEnv('VUE_APP_BE_SERVICE'),
   headers: {
     "content-type": "application/json"
   }
